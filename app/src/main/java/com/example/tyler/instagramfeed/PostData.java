367174643f6bd1928a12714ddb18635c62170400
Tyler Pearson <tylerthewonderfull@gmail.com>
2018-08-22 13:13:38 -0700

    finished lab

--- app/src/main/java/com/example/tyler/instagramfeed/PostData.java
+++ app/src/main/java/com/example/tyler/instagramfeed/PostData.java
@@ -9,7 +9,7 @@ public class PostData {
         witcherprovider.add(new Post("xXx_WitcherBoi_xXx", "Epic Geralt", "https://i.imgur.com/lTv5uiA.jpg"));
         witcherprovider.add(new Post("xXx_WitcherBoi_xXx", "Ciri Cosplay", "https://i.imgur.com/1RyEoxe.jpg"));
         witcherprovider.add(new Post("xXx_WitcherBoi_xXx", "Completion Times", "https://i.imgur.com/UMY9F2A.png"));
-        witcherprovider.add(new Post("xXx_WitcherBoi_xXx", "Game of the year awards", "https://imgur.com/gallery/DfEQLaQ"));
+        witcherprovider.add(new Post("xXx_WitcherBoi_xXx", "Geralt drawing", "https://i.imgur.com/eLtDhxw.jpg?1"));
         witcherprovider.add(new Post("xXx_WitcherBoi_xXx", "Geralt in a pole", "https://i.imgur.com/TNVeqEM.jpg"));
         witcherprovider.add(new Post("xXx_WitcherBoi_xXx", "Troll Boi", "https://i.imgur.com/Lyxppbm.jpg"));
         witcherprovider.add(new Post("xXx_WitcherBoi_xXx", "Triss cosplay", "https://i.imgur.com/1WzVhzJ.jpg"));
